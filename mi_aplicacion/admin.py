83516e3e5423b69f05c83356037fd0ab16f658ec
Gildardo <sainzlopezgildardoantonio@gmail.com>
2026-03-10 11:55:26 -0700

    inicial

---
+++ mi_aplicacion/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from mi_aplicacion.models import Escuela,Maestro,Alumno
+
+admin.site.register(Escuela)
+admin.site.register(Maestro)
+admin.site.register(Alumno)
+# Register your models here.
---
+++ mi_aplicacion/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render, redirect
+
+from django.views import View
+
+from mi_aplicacion.models import Escuela
+
+from mi_aplicacion.forms import EscuelaForm
+
+class home(View):
+    def get(self , request):
+        cdx={
+            "titulo":"home",
+            "subtitulo":"Bienvenido a mi primer aplicacion"
+            }
+        return render(request , "home/home.html", cdx)
+    
+class escuela(View):
+    def get(self , request):
+        escuelas = Escuela.objects.all()
+        cdx={
+            "titulo":"Escuelas",
+            "subtitulo":"Lista de escuelas",
+            "escuelas":escuelas
+            }
+        return render(request , "escuelas/escuelas.html", cdx)
+    
+class EscuelaAlta(View):
+    def get(self , request ):
+        form = EscuelaForm()
+        cdx={
+        "titulo":"Escuela",
+        "subtitulo":"Alta Escuela",
+        "form":form
+        }
+        return render(request , 'escuelas/CRUD.html', cdx)
+    
+    def post(self , request):
+        form = EscuelaForm(request.POST , request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('escuelas')
+            return redirect("home")
+    
+class EscuelaEditar(View):
+    def get(self , request , id):
+        escuela = Escuela.objects.filter(id=id).first()
+        form = EscuelaForm(instance=escuela)
+        cdx={
+        "titulo":"Escuela",
+        "subtitulo":"Editar Escuela",
+        "form":form
+        }
+        return render(request , 'escuelas/CRUD.html', cdx)
+    
+    def post(self , request , id):
+        escuela = Escuela.objects.filter(id=id).first()
+        form = EscuelaForm(request.POST , request.FILES , instance=escuela)
+        if form.is_valid():
+            form.save()
+            return redirect('escuelas')
+        return redirect("home")
+
+class EscuelaEliminar(View):
+    def get(self , request , id):
+        escuela = Escuela.objects.filter(id=id).first()
+        form = EscuelaForm(instance=escuela)
+        cdx={
+        "titulo":"Escuela",
+        "subtitulo":"Eliminacion Escuela",
+        "form":form
+        }
+        return render(request , 'escuelas/CRUD.html', cdx)
+    
+    def post(self , request , id):
+        escuela = Escuela.objects.filter(id=id).first()
+        form = EscuelaForm(request.POST , request.FILES , instance=escuela)
+        if form.is_valid():
+            escuela.delete()
+            return redirect('escuelas')
+        return redirect("home")
+    
+        
+class Maestros(View):
+    def get(self , request):
+        maestros = Maestros.objects.all()
+        cdx={
+            "titulo":"Maestro",
+            "subtitulo":"Lista de maestros",
+            "maestros":maestros
+            }
+        return render(request , "maestros/maestros.html", cdx)
+# Create your views here.
